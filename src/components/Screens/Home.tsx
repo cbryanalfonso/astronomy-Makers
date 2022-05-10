6a42d85ee643f3a75bf67b1687916137d77d84ce
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-10 00:43:06 -0500

    Add a Search Bar

--- src/components/Screens/Home.tsx
+++ src/components/Screens/Home.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
-import axios from "axios"; 
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import axios from "axios";
 import "./Home.css";
 import { AstronomyAPI, Result } from "../../Api/Interfaces";
+import { FilterScreen } from "../Filters/FilterScreen";
 
 export const Home = () => {
   const [data, setData] = useState<Result[]>([]);
+  const [busquedaTitle, setBusquedaTitle] = useState('')
   const page = useRef(1);
   const [urlAPI, setUrlAPI] = useState(
     `https://astroapinodejs.herokuapp.com/astronomy?page=1&limit=9`
@@ -24,7 +26,7 @@ export const Home = () => {
       return (
         <div key={i}>
            <p>{datos}</p> 
-          <br />
+          <br />{value:''}
         </div>
       );
     });
@@ -48,10 +50,29 @@ export const Home = () => {
     });
   }; */
 
+  const handleChange = (evt : ChangeEvent<{ value: string }>)=>{
+    setBusquedaTitle(evt?.currentTarget?.value)
+    console.log(evt.target.value);
+    
+
+  }
+
+
+  const enviarDatos = (evt : any)=>{
+    evt.preventDefault();
+    console.log('enviando datos ', busquedaTitle);
+    
+  }
+
   const imprimirAstronomy = () => {
     return data.map(function (datos, i) {
       return (
-        <a href="#" className="linksImage">
+        <a
+          onClick={() => {
+            console.log("as");
+          }}
+          className="linksImage"
+        >
           <li key={i} className="contenedor">
             <img src={datos.hdurl} alt="Image" className="img" />
             <p>{datos.title}</p>
@@ -63,6 +84,32 @@ export const Home = () => {
 
   return (
     <div>
+      <form onSubmit={enviarDatos} >
+        <div className="barraSearch">
+          <input
+            type="text"
+            className="form-control btn-search "
+            placeholder="Search by title"
+            aria-label="Search by Title"
+            value={busquedaTitle}
+            onChange={handleChange}
+          />
+          <button
+            className="btn btn-outline-secondary btn-width btn-primary"
+            type="submit"
+          >
+            Search
+          </button>
+          <button
+            className="btn btn-outline-secondary btn-width  btn-danger"
+            type="button"
+            onClick={() => {setBusquedaTitle('')}}
+          >
+            Clear
+          </button>
+        </div>
+      </form>
+      <FilterScreen />
       <ul className="nav-menu11">{imprimirAstronomy()}</ul>
       <br />
       <div id="outer">
@@ -85,7 +132,7 @@ export const Home = () => {
             className="inner"
             onClick={() => {
               page.current = page.current + 1;
-              console.log(page.current)
+              console.log(page.current);
               setUrlAPI(
                 `https://astroapinodejs.herokuapp.com/astronomy?page=${page.current}&limit=9`
               );
