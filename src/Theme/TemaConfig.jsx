b957772767782f8797edfe0ec1ac7e2272655353
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-10 22:11:21 -0500

    add Components

---
+++ src/Theme/TemaConfig.jsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
+
+const theme = createTheme({
+    palette:{
+        primary:{
+            main: '#FFFF'
+        },
+        background:{
+            default: '#2196f3'
+        },
+        
+    }
+})
+
+export default theme;
