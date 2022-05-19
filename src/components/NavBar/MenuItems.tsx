df1da832ffc8af725098eaaff8b592078de4db83
Bryan Alfonso Cruz Juan <33972336+cbryanalfonso@users.noreply.github.com>
2022-05-19 14:36:52 -0500

    changes

--- src/components/NavBar/MenuItems.tsx
+++ src/components/NavBar/MenuItems.tsx
@@ -9,20 +9,4 @@ export const menuItems = [
         url: '#',
         cName: 'nav-links',
     },
-   /*  {
-        tittle: 'Products',
-        url: '#',
-        cName: 'nav-links',
-    },
-    {
-        tittle: 'Contact Us',
-        url: '#',
-        cName: 'nav-links',
-    },
-    {
-        tittle: 'Sign Up',
-        url: '#',
-        cName: 'nav-links-mobile',
-    }, */
-
 ]
